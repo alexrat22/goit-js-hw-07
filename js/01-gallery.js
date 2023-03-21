b602de77c264b50fbec1c55fca299d9bf36d7805
Alexander Ratushnyi <olexandr.ratushnyi@gmail.com>
2023-03-21 19:19:36 +0200

    create gallery items

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+const cardsMarkup = createGalleryCard(galleryItems);
+
+gallery.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryCard(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>
+    `;
+    })
+    .join("");
+}
